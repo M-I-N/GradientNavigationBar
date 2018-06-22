46e5fcfb9bb2881ff43458ad51b774f926c796c2
Mufakkharul Islam Nayem <mufakkharul.nayem@bjitgroup.com>
2018-06-22 11:06:58 +0600

    Initial Commit

---
+++ GradientNavigationBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GradientNavigationBar
+//
+//  Created by Nayem BJIT on 6/22/18.
+//  Copyright © 2018 BJIT Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
